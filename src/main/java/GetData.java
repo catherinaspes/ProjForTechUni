8e4eda2973cceea9b6578af084f3b80bc7c02443
Katarzyna Węgrzyńska <catherina.spes@gmail.com>
2018-04-26 18:19:18 +0200

    okienko uploadu pliku, wynikiem jest plik jpeg z wykresem

--- src/main/java/GetData.java
+++ src/main/java/GetData.java
@@ -1,45 +1,12 @@
-import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
 
-public class GetData  {
-
-    public static void main(String[] args) {
+public class GetData {
 
 
-        SampleList sampleList = new SampleList();
-
-        String filePath = "C:\\Users\\Kasia\\Desktop\\Zeroline_ZLGd-6-1400_01_R1.txt";
-
-
-        BufferedReader br = null;
-        try {
-            br = new BufferedReader(new FileReader(filePath));
-        } catch (FileNotFoundException e) {
-            System.out.println("Blad wczytywania pliku");
-            e.printStackTrace();
-        }
-        String line = null;
-        try {
-            while ((line = br.readLine()) != null) {
-                String divider[] = line.split(";");
-                Double firstParam = Double.parseDouble(divider[0]);
-                Double secondParam = Double.parseDouble(divider[1]);
-                Double thirdParam = Double.parseDouble(divider[2]);
-                Double fourthParam = Double.parseDouble(divider[3]);
-                Sample sample = new Sample(firstParam,secondParam,thirdParam,fourthParam);
-                sampleList.addSample(sample);
-            }
-        } catch (IOException e) {
-            System.out.println("Blad z wczytywaniem linii");
-            e.printStackTrace();
-        }
-
-        CreatePlot create = new CreatePlot("Wykres testowy");
-        create.drawPlot(sampleList);
-
-    }
 }
+
